a20e60660546c565c15d63c2c60cb83f00d0659f
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-06-06 22:10:27 +0200

    Much faster, through using an earlier event

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-browser.permissions.getAll().then((perms) => {
-	console.log(perms.origins);
-	browser.tabs.onUpdated.addListener(
-		(tabId) => {
-			browser.tabs.remove(tabId);
-			browser.windows.update(browser.windows.WINDOW_ID_CURRENT, {"state": "minimized"});
-		},
-		{
-			"urls": perms.origins
-		}
-	);
-});
+//array of https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/events/UrlFilter
+const webs =  [
+	{hostContains: "support.discord.com"},
+];
+
+browser.webNavigation.onBeforeNavigate.addListener(
+	(details) => {
+		browser.tabs.remove(details.tabId);
+		browser.windows.update(details.windowId, {"state": "minimized"});
+	},
+	{url: webs}
+);
